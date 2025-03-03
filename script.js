8555e4baebdab212ca9149e75a8fd30994e16341
Michael-Papismedov <dvnka2@gmail.com>
2025-03-03 06:53:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+// בודק אם האתר במצב תחזוקה
+const isMaintenanceMode = true; // שנה ל-true אם האתר במצב תחזוקה, אחרת השאר כ-false
+
+if (isMaintenanceMode) {
+  // אם האתר במצב תחזוקה, הפנה את המשתמש לדף תחזוקה
+  window.location.href = "#/"; // שנה את ה-URL לדף התחזוקה שלך
+}
